141bcbcbda9a37996e832041abc04b4be345bcba
manechacon0302 <mane.chacon@propellingtech.com>
2025-10-29 15:49:07 +0100

    STEP 4 - Refactor totalordercustomername.sql to use int_orders_with_customers

--- dbt_optimization_demo/models/totalordercustomername.sql
+++ dbt_optimization_demo/models/totalordercustomername.sql
@@ -1,21 +1,12 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
 SELECT
-  (
-    SELECT
-      c.name
-    FROM
-      `propellingtech-demo-customers.dbt_semantic_layer_demo.dim_customers` c
-    WHERE
-      c.customer_id = o.customer_id
-  ) AS customer_name,
-  SUM(o.order_total) AS total_spent
-FROM
-  (
-    SELECT
-      *
-    FROM
-      `propellingtech-demo-customers.dbt_semantic_layer_demo.fct_orders`
-  ) AS o
-GROUP BY
-  customer_name
-ORDER BY
-  total_spent DESC
+    customer_name,
+    SUM(order_total) AS total_spent
+FROM {{ ref('int_orders_with_customers') }}
+GROUP BY customer_name
+ORDER BY total_spent DESC
