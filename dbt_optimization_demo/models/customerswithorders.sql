f534371443b5c2a547904165c3c683d33ddaa32f
manechacon0302 <mane.chacon@propellingtech.com>
2025-10-29 15:49:30 +0100

    STEP 3 - Refactor customerswithorders.sql to use int_orders_with_customers

--- dbt_optimization_demo/models/customerswithorders.sql
+++ dbt_optimization_demo/models/customerswithorders.sql
@@ -1,16 +1,13 @@
-with orders as (
-    select * from propellingtech-demo-customers.dbt_semantic_layer_demo.fct_orders
-),
-customers as (
-    select * from propellingtech-demo-customers.dbt_semantic_layer_demo.dim_customers
-),
-orders_with_customer as (
-    select
-        o.order_id,
-        o.customer_id,
-        (select c.name from propellingtech-demo-customers.dbt_semantic_layer_demo.dim_customers c where c.customer_id = o.customer_id) as customer_name,
-        o.order_total,
-        o.ordered_at
-    from orders o
-)
-select * from orders_with_customer
+{{
+  config(
+    materialized='view'
+  )
+}}
+
+SELECT
+    order_id,
+    customer_id,
+    customer_name,
+    order_total,
+    ordered_at
+FROM {{ ref('int_orders_with_customers') }}
