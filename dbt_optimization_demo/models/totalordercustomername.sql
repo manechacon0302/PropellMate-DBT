3d0a52019da9a8632ac37c1d230fd83656690204
manechacon0302 <mane.chacon@propellingtech.com>
2025-10-29 21:58:23 +0100

    Refactor totalordercustomername.sql to use staging models

--- dbt_optimization_demo/models/totalordercustomername.sql
+++ dbt_optimization_demo/models/totalordercustomername.sql
@@ -1,21 +1,33 @@
+{{ config(
+    materialized='view'
+) }}
+
+WITH orders AS (
+    SELECT
+        order_id,
+        customer_id,
+        order_total
+    FROM {{ ref('stg_orders') }}
+),
+
+customers AS (
+    SELECT
+        customer_id,
+        name
+    FROM {{ ref('stg_customers') }}
+),
+
+customer_order_totals AS (
+    SELECT
+        c.name AS customer_name,
+        SUM(o.order_total) AS total_spent
+    FROM orders o
+    INNER JOIN customers c ON o.customer_id = c.customer_id
+    GROUP BY c.name
+)
+
 SELECT
-  (
-    SELECT
-      c.name
-    FROM
-      `propellingtech-demo-customers.dbt_semantic_layer_demo.dim_customers` c
-    WHERE
-      c.customer_id = o.customer_id
-  ) AS customer_name,
-  SUM(o.order_total) AS total_spent
-FROM
-  (
-    SELECT
-      *
-    FROM
-      `propellingtech-demo-customers.dbt_semantic_layer_demo.fct_orders`
-  ) AS o
-GROUP BY
-  customer_name
-ORDER BY
-  total_spent DESC
+    customer_name,
+    total_spent
+FROM customer_order_totals
+ORDER BY total_spent DESC
