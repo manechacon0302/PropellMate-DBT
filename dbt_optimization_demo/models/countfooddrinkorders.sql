b0f7dcc526199f3d5580228b64a97d7d6a6d2d58
manechacon0302 <mane.chacon@propellingtech.com>
2025-10-30 14:32:49 +0100

    Refactor countfooddrinkorders model to use staging models

--- dbt_optimization_demo/models/countfooddrinkorders.sql
+++ dbt_optimization_demo/models/countfooddrinkorders.sql
@@ -1,39 +1,53 @@
-WITH food_orders AS (
-  SELECT
-    c.name,
-    COUNT(oi.order_item_id) AS item_count
-  FROM
-    `propellingtech-demo-customers.dbt_semantic_layer_demo.dim_customers` c
-    JOIN `propellingtech-demo-customers.dbt_semantic_layer_demo.fct_orders` o ON c.customer_id = o.customer_id
-    JOIN `propellingtech-demo-customers.dbt_semantic_layer_demo.order_items` oi ON o.order_id = oi.order_id
-  WHERE
-    oi.is_food_item = 1
-  GROUP BY
-    c.name
+WITH orders AS (
+    SELECT * FROM {{ ref('stg_orders') }}
+),
+customers AS (
+    SELECT * FROM {{ ref('stg_customers') }}
+),
+order_items AS (
+    SELECT * FROM {{ ref('stg_order_items') }}
+),
+customer_order_items AS (
+    SELECT
+        c.name,
+        oi.order_item_id,
+        oi.is_food_item,
+        oi.is_drink_item
+    FROM customers c
+    INNER JOIN orders o ON c.customer_id = o.customer_id
+    INNER JOIN order_items oi ON o.order_id = oi.order_id
+),
+food_orders AS (
+    SELECT
+        name,
+        COUNT(order_item_id) AS item_count
+    FROM customer_order_items
+    WHERE is_food_item = 1
+    GROUP BY name
 ),
 drink_orders AS (
-  SELECT
-    c.name,
-    COUNT(oi.order_item_id) AS item_count
-  FROM
-    `propellingtech-demo-customers.dbt_semantic_layer_demo.dim_customers` c
-    JOIN `propellingtech-demo-customers.dbt_semantic_layer_demo.fct_orders` o ON c.customer_id = o.customer_id
-    JOIN `propellingtech-demo-customers.dbt_semantic_layer_demo.order_items` oi ON o.order_id = oi.order_id
-  WHERE
-    oi.is_drink_item = 1
-  GROUP BY
-    c.name
+    SELECT
+        name,
+        COUNT(order_item_id) AS item_count
+    FROM customer_order_items
+    WHERE is_drink_item = 1
+    GROUP BY name
+),
+combined_orders AS (
+    SELECT
+        name,
+        item_count,
+        'food' AS item_type
+    FROM food_orders
+    UNION ALL
+    SELECT
+        name,
+        item_count,
+        'drink' AS item_type
+    FROM drink_orders
 )
 SELECT
-  name,
-  item_count,
-  'food' AS item_type
-FROM
-  food_orders
-UNION ALL
-SELECT
-  name,
-  item_count,
-  'drink' AS item_type
-FROM
-  drink_orders
+    name,
+    item_count,
+    item_type
+FROM combined_orders
